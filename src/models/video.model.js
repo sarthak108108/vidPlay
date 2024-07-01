bdbbd377b8ccf3622007ad1ef1122c56d84797fd
sarthakarora108108 <sarthakarora108108@gmail.com>
2024-07-01 12:02:29 +0530

    typo in video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -40,4 +40,4 @@ const videoSchema = new Schema({
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
-export const Videos = mongoose.model("Video", videoSchema)
+export const Video = mongoose.model("Video", videoSchema)
